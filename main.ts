f31f485b3245c5232374f07ce4da3210387ab102
Lucas Harada <lucas.harada@trilogy.com>
2023-12-22 05:13:39 +0000

    Initial commit

---
+++ main.ts
@@ -0,0 +1,302 @@
+import { parse } from "csv-parse/sync";
+import mysql from "mysql";
+import fs from "node:fs";
+import { z } from "zod";
+import { execSync } from "node:child_process";
+import * as uuid from "uuid";
+
+function main() {
+  const data = fs.readFileSync("data.csv");
+
+  const parsed = parse(data, {
+    delimiter: ",",
+    columns: true,
+    skip_empty_lines: true,
+  });
+
+  const generatedContentRows: GeneratedContentRow[] = [];
+  const generatedContentAttributeRows: GeneratedContentAttributeRow[] = [];
+
+  let errors = 0;
+  for (const { skill, question, standardIdParams } of skillQuestionPairs(
+    parsed
+  )) {
+    try {
+      const q = parseQuestion(question);
+      const contentId = uuid.v4();
+      generatedContentRows.push({
+        id: contentId,
+        requestedContext: {},
+        content: q,
+        content_generator_config_id: '',
+        standardIdQuery: standardIdQuery(standardIdParams),
+        status: "training",
+      });
+      generatedContentAttributeRows.push({
+        id: uuid.v4(),
+        attributeName: "Skill",
+        attributeValue: skill,
+        generatedContentId: contentId,
+      });
+    } catch (e) {
+      errors++;
+      console.error("Failed to parse question", question);
+    }
+    console.info("--------");
+  }
+  console.error("total errors", errors);
+
+  fs.writeFileSync(
+    "out.sql",
+    [
+      startTransaction(),
+      insertGeneratedContent(generatedContentRows),
+      insertGeneratedContentAttributes(generatedContentAttributeRows),
+      commitTransaction(),
+    ].join(";\n")
+  );
+}
+
+function* skillQuestionPairs(rows: Record<string, string>[]) {
+  const seen = new Set<string>();
+  for (const row of rows) {
+    for (const i of [1, 2, 3, 4, 5, 6]) {
+      for (const j of [1, 2, 3]) {
+        const data = {
+          skill: row[`Skill ${i}`],
+          question: row[`Skill ${i} Question ${j}`],
+          standardIdParams: {
+            standardExternalIdL3: row["Standard Id (L3)"],
+            standardExternalIdL2: row["Standard Id (L2)"],
+            standardExternalIdL1: row["Standard Id (L1)"],
+            clusterExternalId: row["Cluster Id"],
+            domainExternalId: row["Domain Id"],
+            courseName: row["Course"],
+            subjectName: row["Subject"],
+          },
+        };
+        const seenKey = `${data.skill}||||${data.question}`;
+        if (data.skill && data.question && !seen.has(seenKey)) {
+          seen.add(seenKey);
+          yield data;
+        }
+      }
+    }
+  }
+}
+
+function startTransaction() {
+  return "BEGIN";
+}
+
+function commitTransaction() {
+  return "COMMIT";
+}
+
+type StandardIdQueryParams = {
+  standardExternalIdL3: string;
+  standardExternalIdL2: string;
+  standardExternalIdL1: string;
+  clusterExternalId: string;
+  domainExternalId: string;
+  courseName: string;
+  subjectName: string;
+};
+
+function standardIdQuery({
+  standardExternalIdL3,
+  standardExternalIdL2,
+  standardExternalIdL1,
+  clusterExternalId,
+  domainExternalId,
+  courseName,
+  subjectName,
+}: StandardIdQueryParams) {
+  const standardExternalId =
+    standardExternalIdL3 !== ""
+      ? standardExternalIdL3
+      : standardExternalIdL2 !== ""
+      ? standardExternalIdL2
+      : standardExternalIdL1;
+  const query = `
+SELECT
+	s.id
+FROM
+	standards s
+	JOIN standard_clusters sc ON s.cluster_id = sc.id
+	JOIN standard_domains sd ON sc.domain_id = sd.id
+	JOIN subjects sub ON sd.subject_id = sub.id
+	JOIN courses c ON c.subject_id = sub.id
+WHERE
+	s.external_id = ?
+	AND sc.external_id = ?
+	AND sd.external_id = ?
+	AND c.name = ?
+	AND sub.name = ?
+  `
+    .replace(/\n/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+
+  const formattedQuery = mysql.format(query, [
+    standardExternalId,
+    clusterExternalId,
+    domainExternalId,
+    courseName,
+    subjectName,
+  ]);
+  return mysql.raw(`(${formattedQuery}))`);
+}
+
+function parseQuestion(input: string): Question {
+  const stdout = execSync("python parser/main.py", { input }).toString();
+  let res: any;
+  try {
+    res = JSON.parse(stdout);
+  } catch (e) {
+    console.log(stdout);
+    throw e;
+  }
+  const question = {
+    question: res["question_header"]
+      ? res["question_header"] + res["question_body"]
+      : res["question_body"],
+    answer_options: ["A", "B", "C", "D"].map((id) => ({
+      id,
+      answer: res[`option_${id}_answer`],
+      correct: res[`option_${id}_correct`]?.trim() === "True",
+      explanation: res[`option_${id}_explanation`],
+    })),
+  };
+  if (question.answer_options.every((o) => o.correct === false)) {
+    question.answer_options[0].correct = true;
+  }
+
+  const res2 = Question.safeParse(question);
+  if (!res2.success) {
+    console.error(question);
+    throw new Error("Failed to parse question");
+  }
+  return res2.data;
+}
+
+// function parseQuestionWithSections(input: string): Question {
+//   const sections = input.split("\n\n");
+//   const [questionSection, ...optionSections] = sections;
+//
+//   const questionLines = questionSection.split("\n");
+//   if (questionLines[0] !== "Question:") {
+//     throw new Error("Expected Question: section");
+//   }
+//   if (questionLines.length != 2) {
+//     console.error(questionLines);
+//     throw new Error("Question contains newline");
+//   }
+//   const question = questionLines[1].trim();
+//
+//   const options: QuestionOption[] = [];
+//   for (const [i, option] of ["A", "B", "C", "D"].entries()) {
+//     const optionLines = sections[i + 1].split("\n");
+//     if (optionLines[0] !== `Option ${option}:`) {
+//       throw new Error(`Expected ${option}: section`);
+//     }
+//     if (optionLines.length != 4) {
+//       console.error(optionLines);
+//       throw new Error("Option contains newline");
+//     }
+//
+//     if (!optionLines[1].startsWith("Answer:")) {
+//       throw new Error(`Expected Answer: section`);
+//     }
+//     const answer = optionLines[1].replace("Answer:", "").trim();
+//
+//     if (!optionLines[2].startsWith("Explanation:")) {
+//       throw new Error(`Expected Explanation: section`);
+//     }
+//     const explanation = optionLines[2].replace("Explanation:", "").trim();
+//
+//     if (!optionLines[3].startsWith("Correct:")) {
+//       throw new Error(`Expected Correct: section`);
+//     }
+//     const correct = optionLines[3].replace("Correct:", "").trim();
+//     if (correct !== "True" && correct !== "False") {
+//       throw new Error(`Expected True or False for correct`);
+//     }
+//
+//     options.push({
+//       id: option,
+//       answer,
+//       explanation,
+//       correct: correct === "True",
+//     });
+//   }
+//
+//   return Question.parse({
+//     question,
+//     answer_options: options,
+//   });
+// }
+
+const QuestionOption = z.object({
+  id: z.string().min(1),
+  answer: z.string().min(1),
+  correct: z.boolean(),
+  explanation: z.string().default(""),
+});
+type QuestionOption = z.infer<typeof QuestionOption>;
+
+const Question = z.object({
+  question: z.string().min(1),
+  answer_options: z.array(QuestionOption).length(4),
+});
+type Question = z.infer<typeof Question>;
+
+type GeneratedContentRow = {
+  id: string;
+  requestedContext: {}; // Always set this to an empty json object
+  content: Question;
+  content_generator_config_id: ""; // TBD when the other PR is merged
+  standardIdQuery: ReturnType<typeof mysql.raw>;
+  status: "training";
+};
+
+function insertGeneratedContent(rows: GeneratedContentRow[]) {
+  return mysql.format(
+    "INSERT INTO `content_gen_generated_content` (`id`, `content`, `content_generator_config_id`, `standard_id`, `status`) VALUES ?",
+    [
+      rows.map((row) => [
+        row.id,
+        JSON.stringify(row.content),
+        row.content_generator_config_id,
+        row.standardIdQuery,
+        row.status,
+      ]),
+    ]
+  );
+}
+
+type GeneratedContentAttributeRow = {
+  id: string;
+  attributeName: "Skill";
+  attributeValue: string;
+  generatedContentId: string;
+};
+
+function insertGeneratedContentAttributes(
+  rows: GeneratedContentAttributeRow[]
+) {
+  return mysql.format(
+    "INSERT INTO content_gen_generated_content_attributes (id, attribute_name, attribute_value, attribute_hash, generated_content_id) VALUES ?",
+    [
+      rows.map((row) => [
+        row.id,
+        row.attributeName,
+        row.attributeValue,
+        mysql.raw(`SHA2(${mysql.escape(row.attributeValue)}, 256)`),
+        row.generatedContentId,
+      ]),
+    ]
+  );
+}
+
+main();
---
+++ parser/main.py
@@ -0,0 +1,161 @@
+import pyparsing as pp
+import sys
+import re
+from pprint import pprint
+import json
+
+
+def loose_question():
+    question = pp.rest_of_line().set_results_name("question_body")
+
+    def build_option(letter: str):
+        return (
+            pp.LineStart()
+            + pp.CaselessLiteral(letter)
+            + pp.ZeroOrMore(pp.one_of(") ."))
+            + pp.ZeroOrMore(pp.White())
+            + pp.rest_of_line().set_results_name(f"option_{letter}_answer")
+        )
+
+    return (
+        question
+        + build_option("A")
+        + build_option("B")
+        + build_option("C")
+        + build_option("D")
+    )
+
+
+def structured_question():
+    question_header = (
+        pp.SkipTo("Question").set_results_name("question_header")
+        + "Question"
+        + pp.Optional(":")
+        + pp.Optional(pp.LineEnd())
+    )
+    question_body = pp.SkipTo("Option A:").set_results_name("question_body")
+    question = question_header + question_body
+
+    def build_option(letter: str):
+        explanation = (
+            "Explanation:"
+            + pp.Optional(
+                pp.White()
+                + pp.MatchFirst(
+                    [
+                        pp.Literal("Correct")
+                        .set_parse_action(lambda: "True")
+                        .set_results_name(f"option_{letter}_correct"),
+                        pp.Literal("Incorrect")
+                        .set_parse_action(lambda: "False")
+                        .set_results_name(f"option_{letter}_correct"),
+                    ]
+                )
+                + "."
+            )
+            + pp.SkipTo(pp.LineEnd())
+            .set_parse_action(parse_explanation)
+            .set_results_name(f"option_{letter}_explanation")
+            + pp.LineEnd()
+        )
+
+        return (
+            f"Option {letter}:"
+            + pp.Suppress(pp.LineEnd())
+            + "Answer:"
+            + pp.rest_of_line().set_results_name(f"option_{letter}_answer")
+            + pp.Optional(explanation)
+            + pp.Optional(
+                "Correct:"
+                + pp.ZeroOrMore(pp.White())
+                + pp.rest_of_line().set_results_name(f"option_{letter}_correct")
+            )
+        )
+
+    return (
+        question
+        + build_option("A")
+        + build_option("B")
+        + build_option("C")
+        + build_option("D")
+    )
+
+
+def parse_explanation(explanation):
+    if re.search(r"Correct.*?\..+", explanation[0]):
+        return "True"
+    if re.search(r"Incorrect.*?\..+", explanation[0]):
+        return "False"
+    return explanation
+
+
+input0 = """Question:
+As originally ratified, the United States Constitution provided for
+
+Option A:
+Answer:a presidential Cabinet
+Explanation:
+Correct: False
+
+Option B:
+Answer:a two-term presidential limit
+Explanation:
+Correct: False
+
+Option C:
+Answer:an electoral college
+Explanation:
+Correct: True
+
+Option D:
+Answer:political parties
+Explanation:
+Correct: False"""
+
+input1 = """How did the Industrial Revolution affect the way products were made?
+A) Goods were made by hand in small shops.
+B) Factories used machines to make goods faster and cheaper.
+C) People stopped buying things.
+D) Products were only made at home.
+"""
+
+input2 = """Why did cities grow during industrialization?
+A. Because cities had better schools.
+B. Because there were more parks in the cities.
+C. More job opportunities in factories attracted people.
+D. People wanted to live farther from their work."""
+
+input3 = """Why is Dorothea Dix a significant figure in U.S. history?
+A She was a leader in the fight for the abolition of slavery.
+B She led a reform effort to improve working conditions in factories.
+C She was a leader in the fight for women’s suffrage.
+D She led a reform effort to improve care for the mentally ill."""
+
+input4 = """Question:
+Cholesterol is an important component of animal cell membranes. Cholesterol molecules are often delivered to body cells by the blood, which transports the molecules in the form of cholesterol-protein complexes. The complexes must be moved into the body cells before the cholesterol molecules can be incorporated into the phospholipid bilayers of cell membranes.
+
+Based on the information presented, which of the following is the most likely explanation for a buildup of cholesterol molecules in the blood of an animal?
+
+Option A:
+Answer:The animal’s body cells are defective in exocytosis.
+Explanation:Incorrect. The cholesterol is delivered to the body cells by the blood and most likely taken up by endocytosis. If the animal’s body cells were defective in exocytosis, the cholesterol might build up inside the cells but not in the blood.
+
+Option B:
+Answer:The animal’s body cells are defective in endocytosis.
+Explanation:Correct. The cholesterol-protein complexes are most likely moved into the body cells by endocytosis. A defect in endocytosis is likely to result in a buildup of cholesterol in the blood.
+
+Option C:
+Answer:The animal’s body cells are defective in cholesterol synthesis.
+Explanation:Incorrect. Based on the information presented, the body cells take up cholesterol from the blood and do not synthesize cholesterol. A defect in the ability of the animal’s body cells to synthesize cholesterol would not explain a buildup of cholesterol in the blood.
+
+Option D:
+Answer:The animal’s body cells are defective in phospholipid synthesis.
+Explanation:Incorrect. The phospholipid bilayers of the animal’s body cells are most likely intact and functioning properly. A defect in the ability of the animal’s body cells to synthesize phospholipids would not explain a buildup of cholesterol in the blood."""
+
+
+if __name__ == "__main__":
+    parser = pp.MatchFirst([structured_question(), loose_question()])
+
+    res = parser.parse_string(sys.stdin.read())
+    print(json.dumps(res.as_dict()))
+    # pprint(res.as_dict())
